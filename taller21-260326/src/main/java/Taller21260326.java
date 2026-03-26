679bd9ecbccbc26c735c8d60098a6d4e353357f9
Esteban Sanabria <est.juane.sanabria@unimilitar.edu.co>
2026-03-26 10:27:08 -0500

    se solicita el valor n al usuario, error: muestra el mensaje y deja ingresar el numero pero no se ejecuta correctamente ni muestra ninguna salida

--- taller21-260326/src/main/java/Taller21260326.java
+++ taller21-260326/src/main/java/Taller21260326.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
-public class Taller21060326 {
+public class Taller21260326 {
     public static void main(String[] args) {
-
+        System.out.println("Ingrese un valor");
         Scanner sc = new Scanner(System.in);
 
-        
         int n = sc.nextInt();
 
         
         ListaSimple lista = new ListaSimple();
 
-       
+        
         for (int i = 0; i < n - 1; i++) {
             int valor = sc.nextInt();
             lista.insertar(valor);
         }
+    }       
+
     }
-}
